31442c7f633035c2d0f193cc92ce2c752bbde394
Jacob Pearson <JacobPearsonPro@gmail.com>
2021-10-05 20:13:28 -0500

    index.js update2

--- components/views/Home.js
+++ components/views/Home.js
@@ -3,8 +3,8 @@ export default st => `
     <h2>Savvy Coders Jan. 2020 Cohort</h2>
     <a href="">"Call to Action" "Button"</a>
 
-Weather in ${st.weather.city} ${kelvinToFahrenheit(st.weather.temp)}F, feels
-    like ${kelvinToFahrenheit(st.weather.feelsLike)}F</section>`;
+<p> Weather in ${st.weather.city} ${kelvinToFahrenheit(st.weather.temp)}F, feels
+    like ${kelvinToFahrenheit(st.weather.feelsLike)}F</p> </section>`;
 
 const kelvinToFahrenheit = kelvinTemp =>
   Math.round((kelvinTemp - 273.15) * (9 / 5) + 32);
