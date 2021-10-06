b9150b6f0d43e5f7314bee540713c7cf8f0a2ea6
Jacob Pearson <JacobPearsonPro@gmail.com>
2021-10-06 18:41:18 -0500

    Add new APIs

--- index.js
+++ index.js
@@ -3,21 +3,18 @@ import * as state from "./store";
 import Navigo from "navigo";
 import { capitalize } from "lodash";
 import axios from "axios";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const router = new Navigo(window.location.origin);
 
 router.hooks({
   before: (done, params) => {
-    const page =
-      params && params.hasOwnProperty("page")
-        ? capitalize(params.page)
-        : "Home";
-
+    const page = params && params.hasOwnProperty("page") ? capitalize(params.page) : "Home";
     if (page === "Home") {
       axios
-        .get(
-          `https://api.openweathermap.org/data/2.5/weather?appid=fbb30b5d6cf8e164ed522e5082b49064&q=st.%20louis`
-        )
+        .get(`https://api.openweathermap.org/data/2.5/weather?appid=${process.env.OPEN_WEATHER_MAP_API_KEY}&q=st.%20louis`)
         .then(response => {
           state.Home.weather = {};
           state.Home.weather.city = response.data.name;
@@ -27,6 +24,17 @@ router.hooks({
           done();
         })
         .catch(err => console.log(err));
+    }
+    if (page === "Pizza") {
+      axios
+        .get(`${process.env.PIZZA_PLACE_API_URL}`)
+        .then(response => {
+          state.Pizza.pizzas = response.data;
+          done();
+        })
+        .catch(error => {
+          console.log("It puked", error);
+        });
     }
   }
 });
